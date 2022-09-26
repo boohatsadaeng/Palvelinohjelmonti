8e3503443cde1e26bbb07836e879c3b8996b5460
boohatsadaeng <boo.hatsadaeng@gmail.com>
2022-09-26 12:32:04 +0300

    bdffs

--- server.js
+++ server.js
@@ -9,27 +9,12 @@ app.use(bodyparser.urlencoded({extended:false}));
 
 //Muodostetaan tietokantayhteys
 // Luo vakio connectionstringille
-const uri = 'mongodb+srv://demokko:rQzVIcmTCeKg46k9@cluster0.sotfj2v.mongodb.net/bookDb?retryWrites=true&w=majority'
-const uri = 'mongodb+srv://demokko:XC9W0iI5IXSslY10@cluster0.sotfj2v.mongodb.net/bookDb?retryWrites=true&w=majority'
+<<<<<<< HEAD
+const uri = 'mongodb+srv://demokko:RAYTYfNnSu8rfuHo@cluster0.sotfj2v.mongodb.net/bookDb?retryWrites=true&w=majority'
+=======
+
+const uri = 'mongodb+srv://@cluster0.sotfj2v.mongodb.net/bookDb?retryWrites=true&w=majority'
+>>>>>>> fd1d27f0a3b01e5f44701fcf286456275f92ecb2
+
 // Muodostetaan yhteys tietokantaan
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser:true})
-
-@@ -33,6 +33,18 @@ db.once('open', function() {
-    console.log('Tietokantayhteys avattu');
-})
-
-// Kirjoita get-funktio, req.query toimii nyt
-app.get('/books', function(req,res) {
-     // Hae kirjat tietokannasta
-    book.find(req.query, function( err, result) { //tyhjät {} hakuehdossa tuo kaikki, req.query rajaa hakua
-        if (err) {
-            res.send(err)
-        } else {
-            res.send(result);
-        }
-    })
-    })
-
-
-//Laitetaan palvelin kuuntelemaan porttia 8080
-const server = app.listen(8080, function(){});
